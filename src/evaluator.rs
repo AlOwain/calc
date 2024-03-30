be305f8f99adfb4b5b8d6d1a5e4af8df82c20bdd
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2024-03-30 07:37:45 +0300

    Implement `to_postfix` function in evaluator module
    this adds a infix to postfix notation function called by `evalute` that
    will be used in evaluating the operation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,6 +1,38 @@
 use crate::Token;
+use crate::Operator;
 
-pub fn evaluate(tokens: Vec<Token>) -> String {
-    dbg!(tokens);
+fn to_postfix(tokens: Vec<Token>) -> Vec<Token> {
+    let mut stack: Vec<Token> = Vec::with_capacity(tokens.len());
+    let mut operators: Vec<Token> = Vec::new();
+    // FIXME:       Implement the order of operations
+    //          obviously it does not matter right now
+    //          since all we do is + anyway
+    for token in tokens {
+        match &token {
+            Token::Operand(_) => stack.push(token),
+            Token::Operator(operation) => {
+                match operation {
+                    Operator::Add => {
+                        operators.push(token);
+                    }
+                }
+            }
+            _ => {
+                panic!("calc: Error while parsing token");
+            }
+        }
+    }
+
+    for operator in operators {
+        stack.push(operator);
+    }
+
+    stack
+}
+
+pub fn evaluate(operation: Vec<Token>) -> String {
+    let operation = to_postfix(operation);
+    dbg!(operation);
+
     todo!("Implement me!");
 }
