9d61958d6408fa02f79ab79c1a5c2b8261377414
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2024-03-31 14:41:29 +0300

    lexer: Move value flushing into seperate function
    moved pushing tokens into the statement vector and then flushing the
    `token` and `value` variables to their default state.

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,20 +1,24 @@
 use crate::token::*;
 use crate::err;
 
+fn flush(value: String, token: Token, statement: &mut Vec<Token>) -> (String, Token){
+    if !value.is_empty() {
+        statement.push(Token::Operand(Operand::from(value)));
+        return (String::new(), Token::None);
+    }
+    (value, token)
+}
+
 pub fn lexer(args: Vec<String>) -> Vec<Token> {
     let mut statement: Vec<Token> = Vec::new();
 
+    let mut value = String::new();
+    let mut token = Token::None;
     for arg in args.iter() {
-        let mut value = String::new();
-        let mut token = Token::None;
         for byte in arg.as_bytes() {
             match byte {
                 43 => {
-                    if !value.is_empty() {
-                        statement.push(Token::Operand(Operand::from(value)));
-                        token = Token::None;
-                        value = String::new();
-                    }
+                    (value, token) = flush(value, token, &mut statement);
                     if !matches!(token, Token::None) { err!("Error while parsing, invalid token state: {}, {:?}", arg, token); }
 
                     token = Token::Operator(Operator::Add);
@@ -33,9 +37,7 @@ pub fn lexer(args: Vec<String>) -> Vec<Token> {
                 _ => { err!("Invalid token: \'{}\'", arg); }
             }
         }
-        if matches!(token, Token::Operand(_)) {
-            statement.push(Token::Operand(Operand::from(value)))
-        }
+        (value, token) = flush(value, token, &mut statement);
     }
     statement
 }
