aa2abbe73961e119ae0d2b6d8c861505b6753e30
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2024-04-03 13:28:33 +0300

    Implement crude Order of Operations in `to_postfix`
    This adds basic support for the order of operations when converting the
    tokenized input into postfix notation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,31 +1,45 @@
 use crate::token::*;
 use crate::err;
 
+fn push_op(operator: Operator, statement: &mut Vec<Token>, stack: &mut Vec<Token>) {
+    // FIXME:   If the operator is a right parantheses empty the stack up to the left
+    //          bracket and push it into the statement
+    match stack.pop() {
+        Some(stack_top) => {
+            match stack_top {
+                Token::Operator(ref stack_op) => {
+                    if operator > *stack_op {
+                        stack.push(stack_top);
+                        stack.push(Token::Operator(operator));
+                        return;
+                    }
+                    statement.push(stack_top);
+                    push_op(operator, statement, stack);
+                }
+                _ => panic!("Non-Operator was added to Operator only stack")
+            }
+        }
+        None => stack.push(Token::Operator(operator)),
+    }
+}
+
 fn to_postfix(tokens: Vec<Token>) -> Vec<Token> {
     let mut statement: Vec<Token> = Vec::with_capacity(tokens.len());
-    let mut operators: Vec<Token> = Vec::new();
-    // FIXME:       Implement the order of operations
-    //          obviously it does not matter right now
-    //          since all we do is + anyway
+    let mut stack: Vec<Token> = Vec::new();
     for token in tokens {
-        match &token {
+        match token {
             Token::Operand(_) => statement.push(token),
-            Token::Operator(operation) => {
-                match operation {
-                    Operator::Add => {
-                        operators.push(token);
-                    }
-                    Operator::Subtract=> {
-                        operators.push(token);
-                    }
-                    _ => { todo!("Add and Subtract operators are the only types implemented.") }
-                }
+            Token::Operator(operator) => {
+                push_op(operator, &mut statement, &mut stack);
             }
             _ => err!("Error while parsing tokens, token type undefined."),
         }
     }
 
-    statement.append(&mut operators);
+    // TODO: Implement checks
+    //          - If the stack is not empty
+    //          - If the operands are not in the same order
+    statement.append(&mut stack);
     statement
 }
 
