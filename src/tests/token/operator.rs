bdb0ca326773787c244ec19f9d32529c96538cdf
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2024-04-10 00:34:36 +0300

    Add Order of Operations testing for all existing operators
    Add testing for the order of operations in existing operators (test
    currently fails)

--- src/tests/token/operator.rs
+++ src/tests/token/operator.rs
@@ -2,6 +2,34 @@
 use crate::token::*;
 
 #[test]
+fn order_of_operations() {
+    let multiply    = Operator::Multiply;
+    let divide      = Operator::Divide;
+    let add         = Operator::Add;
+    let subtract    = Operator::Subtract;
+
+    assert!(multiply == multiply);
+    assert!(multiply == divide);
+    assert!(multiply > add);
+    assert!(multiply > subtract);
+
+    assert!(divide == multiply);
+    assert!(divide == divide);
+    assert!(divide > add);
+    assert!(divide > subtract);
+
+    assert!(add < multiply);
+    assert!(add < divide);
+    assert!(add == add);
+    assert!(add == subtract);
+
+    assert!(subtract < multiply);
+    assert!(subtract < divide);
+    assert!(subtract == add);
+    assert!(subtract == subtract);
+}
+
+#[test]
 fn operator() {
     let lhs = Operand::Numeric(Numeric {
         value: 1,
