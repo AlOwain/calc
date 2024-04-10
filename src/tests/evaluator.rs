cd10ced450fcfbe2fc2de7928a060347eb54990d
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2024-04-10 04:31:19 +0300

    Evalutor test case generator script included in comment.

--- src/tests/evaluator.rs
+++ src/tests/evaluator.rs
@@ -2,6 +2,8 @@
 use crate::evaluator;
 use crate::token::*;
 
+// To generate similar test cases, use the following snippet:
+// https://gist.github.com/AlOwain/1885b961a219a483e7d030f0929e8369
 #[test]
 fn evaluator() {
     let equation = vec!(
