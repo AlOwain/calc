cb5797cc92ec443dd48ea2882711110caac5912d
Abdulaziz AlOwain <abdulaziz@alowain.dev>
2024-04-27 05:27:33 +0300

    Create `multi_digit_evaluator` and `numeric_lexer`; rename `lexer` tests
    ...which introduces numeric test on digits >10 to avoid incorrect lexing
    of values

--- src/tests/lexer.rs
+++ src/tests/lexer.rs
@@ -3,7 +3,13 @@ use crate::lexer;
 use crate::token::*;
 
 #[test]
-fn lexer() {
+fn numeric_lexer() {
+    let result = lexer::lexer(vec!("1391303".to_string()));
+    assert_eq!(result, vec!(Token::Operand(Operand::Numeric(1391303))));
+}
+
+#[test]
+fn digit_lexer() {
     let result = lexer::lexer(vec!("1".to_string()));
     assert_eq!(result, vec!(Token::Operand(Operand::Numeric(1))));
 }
